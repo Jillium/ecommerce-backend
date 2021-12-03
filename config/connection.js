4f6bad09f4597cc0713711b85a13107539c3d8d4
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-03 10:29:21 -0700

    sql and server connection working

--- config/connection.js
+++ config/connection.js
@@ -2,14 +2,22 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+
+let sequelize = process.env.JAWSDB_URL
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL)
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    port: 3306,
+    dialect: 'mysql',
+    dialectOptions: {
+      decimalNumbers: true,
+    }
+  });
+
+}
+
+
 
 module.exports = sequelize;
