ebf0cab6e78760482197bc966d7e19adebbc5cd0
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-03 15:01:58 -0700

    product get all tested and working, need to do other routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,8 +6,38 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  Product.findAll()
-  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: [
+      'id',
+      'product_name',
+      'price',
+      'stock',
+      'category_id'
+    ],
+    include: [
+      // be sure to include its associated Category and Tag data
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(productData => {
+    if (!productData) {
+      res.status(404).json({ message: "No data found" });
+      return;
+    }
+    res.json(productData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json({ message: "There has been an error" })
+  });
+  
 });
 
 // get one product
