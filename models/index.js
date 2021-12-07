d45503828d99eb016d0a0474ced1b7ca9889c56f
Jillium <86424914+Jillium@users.noreply.github.com>
2021-12-07 13:24:58 -0700

    category delete route working

--- models/index.js
+++ models/index.js
@@ -6,24 +6,28 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
-  foreignKey: 'tag_id'
+  foreignKey: 'tag_id',
+  onDelete: 'CASCADE'
 });
 
 module.exports = {
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -97,13 +97,14 @@ router.delete('/:id', (req, res) => {
   })
   .then(categoryData => {
     if (!categoryData) {
-      res.status(404).json({ message: "No product found with this id "});
+      res.status(404).json({ message: "No category found with this id "});
       return;
     }
-    res.json({ message: "The product has been deleted! "});
+    res.json({ message: "The category has been deleted! "});
   })
   .catch(err => {
     res.status(500).json({ message: "There has been an error" })
+    console.log(err)
   })
 });
 
